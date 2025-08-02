95df8c4a7f1078b16089153d7a5d3dcaecf45360
Vvk_0105 <vivekbabu.sics@gmail.com>
2025-08-02 22:35:39 +0530

    loading added

--- scripts.js
+++ scripts.js
@@ -386,3 +386,13 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   }
 });
+
+window.addEventListener("load", () => {
+  const loader = document.getElementById("loader");
+  loader.style.opacity = "0";
+  loader.style.transition = "opacity 0.5s ease";
+
+  setTimeout(() => {
+    loader.style.display = "none";
+  }, 500);
+});
